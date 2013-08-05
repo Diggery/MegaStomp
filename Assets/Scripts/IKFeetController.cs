957c36052bcf3afee7eb00e1f9d6f4fc6bfeb6dd
Brian <collins.brian@gmail.com>
2013-08-04 20:30:02 -0700

    dhgng

--- Assets/Scripts/IKFeetController.cs
+++ Assets/Scripts/IKFeetController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class IKFeetController : MonoBehaviour {
 	
 	public Transform body;
+	Vector3 lastBodyPos;
 	
 	public Transform leftFootPrint;
 	public Transform leftFootGoal;
@@ -16,112 +17,141 @@ public class IKFeetController : MonoBehaviour {
 	public bool leftFootUp;
 	public bool rightFootUp;
 	
+	public bool moving;
+	
 	public float stanceWidth = 0.5f;
-	public float stanceOffset = 0.5f;
 	public float motionOffset = 1.2f;
 	public float stepDistance = 1.2f;
 	public float stepHeight = 1.2f;
 	public float legLength = 2.3f;
 	public AnimationCurve footLiftCurve;
+	public AnimationCurve footTiltCurve;
 	
 	float leftFootStepTimer = 0.0f;
 	float rightFootStepTimer = 0.0f;
 	
 	void Start () {
-	
+		lastBodyPos = body.position;
+		leftFootGoal.renderer.enabled = false;
+		rightFootGoal.renderer.enabled = false;
 	}
 	
-	void Update () {
-		
-		float bodyDelta = getBodyDelta();
-		float stepThreshold = 0.0f;
-		
+	void FixedUpdate () {
+				
 		float currentStanceWidth;
-		float currentStanceOffset;
+		float currentStepDistance;
+
+		currentStanceWidth = stanceWidth;
+		currentStepDistance = stepDistance;
 		
-//		if (Mathf.Abs(bodyDelta) > 0.1) {
-//			currentStanceWidth = stanceWidth * 0.5f ;
-//			currentStanceOffset = 0.0f;
-//			stepThreshold = stepDistance;
-//		} else {
-			currentStanceWidth = stanceWidth;
-			currentStanceOffset = stanceOffset;			
-			stepThreshold = stepDistance;// * 0.5f;
-		//}
+		float currentSpeed = getSpeed() * 10;
 		
-		leftFootPrint.localPosition = new Vector3(-currentStanceWidth, 0.0f, currentStanceOffset);
-		rightFootPrint.localPosition = new Vector3(currentStanceWidth, 0.0f, -currentStanceOffset);
+		//moving = currentSpeed > 0.5f ? true : false;
+		currentStanceWidth = Mathf.Lerp (stanceWidth, 0.0f, currentSpeed -0.5f);
+		
+	//	leftFootPrint.localPosition = new Vector3(-currentStanceWidth, 0.0f, currentStanceOffset);
+	//	rightFootPrint.localPosition = new Vector3(currentStanceWidth, 0.0f, -currentStanceOffset);
 		
 		if (!leftFootUp && !rightFootUp) {
 			float rightFootDistance = Vector3.Distance(rightFootPrint.position, rightFootGoal.position);
 			float leftFootDistance = Vector3.Distance(leftFootPrint.position, leftFootGoal.position);
 			
-			if (leftFootDistance > stepThreshold && rightFootDistance > stepThreshold) {
+			if (leftFootDistance > currentStepDistance && rightFootDistance > currentStepDistance) {
 				if (leftFootDistance > rightFootDistance) {
 					liftLeftFoot();
 				} else {
 					liftRightFoot();
 				}
 			} else {
-				if (leftFootDistance > stepThreshold) liftLeftFoot(); 	
-				if (rightFootDistance > stepThreshold) liftRightFoot(); 	
+				if (leftFootDistance > currentStepDistance) liftLeftFoot(); 	
+				if (rightFootDistance > currentStepDistance) liftRightFoot(); 	
 			}
 		}
 		
-		float momentum = 0.0f;
-		
-		if (Mathf.Abs(bodyDelta) > 0.1f) {
-			momentum = bodyDelta * 2;
-		}
-		Vector3 stepOffset = new Vector3(0.0f, 0.0f, momentum );
-		
-		Vector3 newRightFootPos = Vector3.zero;
-		if (rightFootUp) {
-			rightFootStepTimer += Time.deltaTime * 3;
-			if (rightFootStepTimer > 1.0f) plantRightFoot();
-			newRightFootPos = Vector3.Lerp(rightFootOldPos, rightFootPrint.position + stepOffset, rightFootStepTimer);
-			newRightFootPos.y += footLiftCurve.Evaluate(rightFootStepTimer) * stepHeight;
-			rightFootGoal.position = newRightFootPos;
-		}
-		
+		//position the Left Foot
 		Vector3 newLeftFootPos = Vector3.zero;
 		if (leftFootUp) {
 			leftFootStepTimer += Time.deltaTime * 3;
 			if (leftFootStepTimer > 1.0f) plantLeftFoot();
-			newLeftFootPos = Vector3.Lerp(leftFootOldPos, leftFootPrint.position + stepOffset, leftFootStepTimer);
+			newLeftFootPos = Vector3.Lerp(leftFootOldPos, leftFootPrint.position, leftFootStepTimer);
 			newLeftFootPos.y += footLiftCurve.Evaluate(leftFootStepTimer) * stepHeight;
 			leftFootGoal.position = newLeftFootPos;
 		}
 		
-		float pelvisHeight = legLength + ((newLeftFootPos.y + newRightFootPos.y) * 0.75f) ;
-		body.position = Vector3.Lerp (body.position, transform.position + new Vector3(0.0f, pelvisHeight, 0.0f), Time.deltaTime * 8);
+		//position the Right Foot
+		Vector3 newRightFootPos = Vector3.zero;
+		if (rightFootUp) {
+			rightFootStepTimer += Time.deltaTime * 3;
+			if (rightFootStepTimer > 1.0f) plantRightFoot();
+			newRightFootPos = Vector3.Lerp(rightFootOldPos, rightFootPrint.position, rightFootStepTimer);
+			newRightFootPos.y += footLiftCurve.Evaluate(rightFootStepTimer) * stepHeight;
+			rightFootGoal.position = newRightFootPos;
+		}
+		
+		//position the pelvis
+		Vector3 pelvisTarget;
+		
+		if (moving) {
+			float pelvisHeight = legLength + ((newLeftFootPos.y + newRightFootPos.y) * 0.75f);
+			pelvisTarget = transform.position + new Vector3(0.0f, pelvisHeight, 0.0f);
+		} else {
+			Vector3 MidFootPos = Vector3.Lerp(rightFootGoal.position, leftFootGoal.position, 0.5f);
+			MidFootPos.y += legLength;
+			pelvisTarget = MidFootPos;
+		}
+		body.position = Vector3.Lerp (body.position, pelvisTarget, Time.deltaTime * 10);
+		body.rotation = Quaternion.Lerp (body.rotation, transform.rotation, Time.deltaTime * 3);
 	}
 	
 	void liftLeftFoot() {
 		leftFootUp = true;
 		leftFootOldPos = leftFootGoal.position;
+		Vector3 bodyRelative = transform.InverseTransformPoint(leftFootGoal.position);
+		float currentOffset = Mathf.Clamp(-bodyRelative.z, -stepDistance * 0.8f, stepDistance * 0.8f);
+		Vector3 localPrintPos = leftFootPrint.localPosition;
+		localPrintPos.z = currentOffset;
+		leftFootPrint.localPosition = localPrintPos;
 	}
 	
 	void plantLeftFoot() {
 		leftFootUp = false;
 		leftFootStepTimer = 0;
 		leftFootOldPos = leftFootGoal.position;
+		Vector3 localPrintPos = leftFootPrint.localPosition;
+		localPrintPos.z = 0.0f;
+		leftFootPrint.localPosition = localPrintPos;
 	}
 	
 	void liftRightFoot() {
 		rightFootUp = true;
 		rightFootOldPos = rightFootGoal.position;
+		Vector3 bodyRelative = transform.InverseTransformPoint(rightFootGoal.position);
+		float currentOffset = Mathf.Clamp(-bodyRelative.z, -stepDistance * 0.8f, stepDistance * 0.8f);
+		Vector3 localPrintPos = rightFootPrint.localPosition;
+		localPrintPos.z = currentOffset;
+		rightFootPrint.localPosition = localPrintPos;
 	}
 	
 	void plantRightFoot() {
 		rightFootUp = false;
 		rightFootStepTimer = 0;
 		rightFootOldPos = rightFootGoal.position;
+		Vector3 localPrintPos = rightFootPrint.localPosition;
+		localPrintPos.z = 0.0f;
+		rightFootPrint.localPosition = localPrintPos;
 	}
 	
-	float getBodyDelta() {
-		Vector3 bodyRelative = body.InverseTransformPoint(transform.position);
-
-		return Mathf.Clamp(bodyRelative.z * 0.3f, -motionOffset, motionOffset);
+	private float getSpeed() {
+		Vector3 bodyDelta = lastBodyPos - body.position;
+		lastBodyPos = body.position;
+		return bodyDelta.magnitude;
+	}
+	
+	public bool isRightFootUp() {
+		return rightFootUp;
+	}
+	
+	public bool isLeftFootUp() {
+		return leftFootUp;
 	}
 }
--- Assets/Scripts/IKLegController.cs
+++ Assets/Scripts/IKLegController.cs
@@ -3,27 +3,30 @@ using System.Collections;
 
 public class IKLegController : MonoBehaviour {
 	
-	public float hipLength;
-	public float kneeLength;
+	float hipLength;
+	float kneeLength;
 	
 	public Transform upperLeg;
-	public Transform knee;
 	public Transform lowerLeg;
 	public Transform foot;
 	public Transform IKEnd;
 	public Transform goal;
-	public bool rightFoot;
+	public bool rightLeg;
+	
+	public Transform feetController;
+	IKFeetController ikFeetController;
 
 
 	void Start () {
+		if (feetController) ikFeetController = feetController.GetComponent<IKFeetController>();
 		hipLength = lowerLeg.localPosition.magnitude;
-		kneeLength = foot.localPosition.magnitude;	
+		kneeLength = Vector3.Distance(lowerLeg.position, IKEnd.position);
 	}
 	
-	void Update () {
-		float hipOffset = rightFoot ? 15 : -15;
+	void FixedUpdate () {
+		float hipOffset = rightLeg ? 15 : -15;
 
-		Vector3 hipAngle = Quaternion.AngleAxis(hipOffset, Vector3.up) * Vector3.forward;
+		Vector3 hipAngle = Quaternion.AngleAxis(hipOffset + feetController.eulerAngles.y, Vector3.up) * Vector3.forward;
 		
 		transform.LookAt(goal, hipAngle);
 		IKEnd.position = goal.position;
@@ -32,15 +35,23 @@ public class IKLegController : MonoBehaviour {
 		float angle1 = 0.0f;
 		float angle2 = 0.0f;
 
-		
 		IKSolver.CalcIK_2D(out angle1, out angle2, true, hipLength, kneeLength, targetPos.x, targetPos.y);
-		
-		//angle1 -= 90;
-		
+				
 		upperLeg.localRotation = Quaternion.AngleAxis(angle1 * Mathf.Rad2Deg, Vector3.right);
-		knee.localRotation = Quaternion.AngleAxis(angle2 * Mathf.Rad2Deg * 0.75f, Vector3.right);
 		lowerLeg.localRotation = Quaternion.AngleAxis(angle2 * Mathf.Rad2Deg, Vector3.right);
 		
-		foot.rotation = goal.rotation * Quaternion.AngleAxis(90, Vector3.right) * Quaternion.AngleAxis(hipOffset, Vector3.up);
+		bool footUp = false;
+		if (ikFeetController) {
+			if (rightLeg) {
+				footUp = ikFeetController.isRightFootUp();	
+			} else {
+				footUp = ikFeetController.isLeftFootUp();	
+			}
+		}
+		float xOffset = footUp ? 90 : 90;
+		foot.rotation = goal.rotation * 
+			Quaternion.AngleAxis(xOffset, Vector3.right) * 
+			Quaternion.AngleAxis(-hipOffset - feetController.eulerAngles.y , Vector3.forward);// *
+			//Quaternion.AngleAxis(hipOffset + FeetController.eulerAngles.y, Vector3.up);
 	}
 }
